e996cd6fe6253799b4c1b6af10b48f51dc5c1b1c
Purwa Sadr <purwasadr@gmail.com>
2022-08-02 22:04:57 +0700

    feat: change config and refactoring
    * change file url
    * refactoring variable
    * change config naming

--- libs/index.js
+++ libs/index.js
@@ -1,64 +1,62 @@
-const { credential } = require('firebase-admin');
-const { initializeApp } = require('firebase-admin/app');
-const { getStorage } = require('firebase-admin/storage');
+const {credential} = require('firebase-admin');
+const {initializeApp} = require('firebase-admin/app');
+const {getStorage} = require('firebase-admin/storage');
 const uuid = require('uuid');
 
 module.exports = {
     init(config) {
         initializeApp({
             credential: credential.cert(config.serviceAccount),
-            storageBucket: config.bucketUrl,
-            ...config.options
+            storageBucket: config.storageBucket,
+            ...config.options,
         });
 
-        const storageBucket = getStorage().bucket(config.customBucket);
+        const storage = getStorage().bucket(config.customBucket);
 
-        const upload = (file, customParams = {}) => new Promise((resolve, reject) => {
-            const path = file.path ? `${file.path}/` : '';
-            const filename = `${path}${file.name}`
-            const token = uuid.v4();
+        const upload = (file, customParams = {}) =>
+            new Promise((resolve, reject) => {
+                const path = file.path ? `${file.path}/` : '';
+                const filename = `${path}${file.name}`;
+                const token = uuid.v4();
 
-            storageBucket.file(filename)
-                .save(Buffer.from(file.buffer, "binary"), { 
-                    public: true,
-                    contentType: file.mime,
-                    metadata: {
+                storage.file(filename).save(
+                    Buffer.from(file.buffer, 'binary'),
+                    {
+                        public: true,
+                        contentType: file.mime,
                         metadata: {
-                            firebaseStorageDownloadTokens: token
-                        }
+                            metadata: {
+                                firebaseStorageDownloadTokens: token,
+                            },
+                        },
+                        ...customParams,
                     },
-                    ...config.uploadOptions,
-                }, (err) => {
-                    if (err) {
-                        return reject(err);
+                    (err) => {
+                        if (err) {
+                            return reject(err);
+                        }
+
+                        file.url = `https://storage.googleapis.com/${config.storageBucket}/${filename}`;
+                        resolve();
                     }
-
-                    file.url = createPersistentDownloadUrl(config.bucketUrl, filename, token);
-                    resolve();
-                })
-
-        })
+                );
+            });
 
         return {
             upload: (file, customParams = {}) => upload(file, customParams),
-            delete: (file, customParams = {}) => new Promise((resolve, reject) => {
-                const path = file.path ? `${file.path}/` : '';
-                const filename = `${path}${file.name}`
+            delete: (file, customParams = {}) =>
+                new Promise((resolve, reject) => {
+                    const path = file.path ? `${file.path}/` : '';
+                    const filename = `${path}${file.name}`;
 
-                storageBucket.file(filename).delete({ ...config.deleteOptions }, (err) => {
-                    if (err) {
-                        reject(err);
-                    }
+                    storage.file(filename).delete({...customParams}, (err) => {
+                        if (err) {
+                            reject(err);
+                        }
 
-                    resolve();
-                })
-            })
-        }
-    }
-}
-
-const createPersistentDownloadUrl = (bucket, pathToFile, downloadToken) => {
-    return `https://firebasestorage.googleapis.com/v0/b/${bucket}/o/${encodeURIComponent(
-      pathToFile
-    )}?alt=media&token=${downloadToken}`;
-  };
+                        resolve();
+                    });
+                }),
+        };
+    },
+};
